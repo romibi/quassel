fda500e97a3ab6661535bffa2317262911a7e661
Svetlana Tkachenko <svetlana@members.fsf.org>
2018-01-31 23:11:35 +0000

    exclude ignored messages (maybe Closes #1472)
    untested

--- src/qtui/awaylogfilter.cpp
+++ src/qtui/awaylogfilter.cpp
@@ -18,6 +18,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
  ***************************************************************************/
 
+#include "clientignorelistmanager.h"
 #include "awaylogfilter.h"
 
 AwayLogFilter::AwayLogFilter(MessageModel *model, QObject *parent)
@@ -33,16 +34,25 @@ bool AwayLogFilter::filterAcceptsRow(int sourceRow, const QModelIndex &sourcePar
     QModelIndex source_index = sourceModel()->index(sourceRow, 0);
 
     Message::Flags flags = (Message::Flags)sourceModel()->data(source_index, MessageModel::FlagsRole).toInt();
+    // Only show highlights from the backlog
     if (!(flags & Message::Backlog && flags & Message::Highlight))
         return false;
 
+    // do not use invalid buffers
     BufferId bufferId = sourceModel()->data(source_index, MessageModel::BufferIdRole).value<BufferId>();
     if (!bufferId.isValid()) {
         return false;
     }
 
+    // only show messages that were sent after the user detached
     if (Client::networkModel()->lastSeenMsgId(bufferId) >= sourceModel()->data(source_index, MessageModel::MsgIdRole).value<MsgId>())
         return false;
+    
+    // ignorelist handling
+    // only match if message is not flagged as server msg
+    if (!(flags & Message::ServerMsg) && Client::ignoreListManager()
+        && Client::ignoreListManager()->match(source_index.data(MessageModel::MessageRole).value<Message>(), Client::networkModel()->networkName(bufferId)))
+return false;
 
     return true;
 }
